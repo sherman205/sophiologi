59dacc8177103f53e5894abf009fdcf99f5745cb
Sophia <sherman205@gmail.com>
2019-12-01 16:15:18 -0800

    Add name and social icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react"
 import styled from "styled-components"
+import GithubIcon from './icons/github'
+import LinkedinIcon from './icons/linkedin'
+import InstaIcon from './icons/instagram'
 import { media } from "../styles/media"
 import { theme } from "../styles/theme"
 
 const FooterContainer = styled.footer`
   display: flex;
+  flex-direction: column;
   padding: 15px;
   justify-content: center;
   align-items: center;
@@ -17,8 +21,56 @@ const FooterContainer = styled.footer`
   ${media.tablet`min-height: 100px;`};
 `;
 
+const LogoText = styled.div`
+  font-size: 25px;
+  color: ${theme.colors.clay};
+  ${media.phone`font-size: 20px;`};
+`;
+
+const Social = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const SocialLink = styled.a`
+  width: 100%;
+  padding: 10px;
+  svg {
+    fill: ${theme.colors.lightGrey};
+    height: 20px;
+    width: 20px;
+  }
+`;
+
 const Footer = () => (
     <FooterContainer>
+      <LogoText>Sophiologi</LogoText>
+      <Social>
+        <SocialLink
+          href="https://github.com/sherman205"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          aria-label="github link"
+        >
+          <GithubIcon />
+        </SocialLink>
+        <SocialLink
+          href="https://www.linkedin.com/in/sophiasherman/"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          aria-label="linkedin link"
+        >
+          <LinkedinIcon />
+        </SocialLink>
+        <SocialLink
+          href="https://www.instagram.com/thealphabeetle"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          aria-label="instagram link"
+        >
+          <InstaIcon />
+        </SocialLink>
+      </Social>
       <div>Designed &amp; built by Sophia Sherman</div>
       <div>
       </div>
